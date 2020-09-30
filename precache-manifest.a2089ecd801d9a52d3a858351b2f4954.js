a147dc8a7d259e8ea8accc6a91b3a32d401e577d
beamwanatvhaya <wanatchaya.co.61@ubu.ac.th>
2020-09-30 10:47:50 +0700

    Updates

---
+++ precache-manifest.a2089ecd801d9a52d3a858351b2f4954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f37c5944634304de91efcc5c954e4a7",
+    "url": "/mathcom/index.html"
+  },
+  {
+    "revision": "e315e415d6973a1b3979",
+    "url": "/mathcom/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19a1e6166864f58ff2f9",
+    "url": "/mathcom/static/js/2.a693db1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mathcom/static/js/2.a693db1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e315e415d6973a1b3979",
+    "url": "/mathcom/static/js/main.11e32121.chunk.js"
+  },
+  {
+    "revision": "8ca9803c333834e2f58e",
+    "url": "/mathcom/static/js/runtime-main.f0788721.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mathcom/static/media/logo.5d5d9eef.svg"
+  }
+]);
